10df4a6f0a17f8d2c3f5d90806d532bf97536fea
Pranav Rajpurkar <pranav.rajpurkar@gmail.com>
2014-04-05 14:35:53 -0700

    adds socket io

--- app.js
+++ app.js
@@ -30,6 +30,18 @@ if ('development' == app.get('env')) {
 app.get('/', function(req,res){
     res.sendfile("public/main.html");
 });
-http.createServer(app).listen(app.get('port'), function(){
+
+var server =http.createServer(app)
+
+server.listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
+});
+
+var io = require('socket.io').listen(server);
+
+io.sockets.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
 });
