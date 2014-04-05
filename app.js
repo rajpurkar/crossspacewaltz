fe5497d53c4a37c0ed686363c9cf4a98a8cb3035
Pranav Rajpurkar <pranav.rajpurkar@gmail.com>
2014-04-05 14:15:34 -0700

    adds basic canvas with kinetic

--- app.js
+++ app.js
@@ -19,17 +19,17 @@ app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
-app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(app.router);
 
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
-app.get('/users', user.list);
-
+app.get('/', function(req,res){
+    res.sendfile("public/main.html");
+});
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
