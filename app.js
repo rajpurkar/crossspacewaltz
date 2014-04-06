ed71f87c89bf9e409440885778ef40e04a25153f
Pranav Rajpurkar <pranav.rajpurkar@gmail.com>
2014-04-05 17:01:43 -0700

    adds sockets for mousePos

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 /**
  * Module dependencies.
  */
@@ -24,24 +23,34 @@ app.use(app.router);
 
 // development only
 if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+	app.use(express.errorHandler());
 }
 
-app.get('/', function(req,res){
-    res.sendfile("public/main.html");
+app.get('/', function(req, res) {
+	res.sendfile("public/main.html");
 });
 
-var server =http.createServer(app)
+var server = http.createServer(app)
 
-server.listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+server.listen(app.get('port'), function() {
+	console.log('Express server listening on port ' + app.get('port'));
 });
 
 var io = require('socket.io').listen(server);
 
-io.sockets.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
+io.sockets.on('connection', function(socket) {
+
+	socket.emit('news', {
+		hello : 'world'
+	});
+
+	socket.on('msg', function(data) {
+		socket.broadcast.emit('lol', {
+			id: socket.id,
+			pos: data
+		});
+		console.log('Chat message by ', socket.id);
+		console.log('Chat message by ', data.x, data.y);
+	});
+
 });
