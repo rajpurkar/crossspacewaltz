fe3ffa0ef1cde19ed368451d02066226a69a890a
Brad Girardeau <brad.girardeau@gmail.com>
2014-04-06 12:20:32 -0700

    Fix viewport with mouse.

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -140,41 +140,39 @@ Camera.prototype.update = function()
 	// keep following the player (or other desired object)
 	if(this.followed != null)
 	{		
-		if(this.axis == "horizontal" || this.axis == "both")
-		{		
-	// moves camera on horizontal axis based on followed object position
-	if(this.followed.x - this.xView  + this.xDeadZone > this.wView)
-		this.xView = this.followed.x - (this.wView - this.xDeadZone);
-	else if(this.followed.x  - this.xDeadZone < this.xView)
-		this.xView = this.followed.x  - this.xDeadZone;
+		if(this.axis == "horizontal" || this.axis == "both") {		
+			// moves camera on horizontal axis based on followed object position
+			if (this.followed.x - this.xView  + this.xDeadZone > this.wView){
+				this.xView = this.followed.x - (this.wView - this.xDeadZone);
+				console.log({xview: this.xView, x : this.followed.x});
+			} else if(this.followed.x  - this.xDeadZone < this.xView){
+				this.xView = this.followed.x  - this.xDeadZone;
+			}
+		}
+		
+		if (this.axis == "vertical" || this.axis == "both") {
+			// moves camera on vertical axis based on followed object position
+			if(this.followed.y - this.yView + this.yDeadZone > this.hView)
+				this.yView = this.followed.y - (this.hView - this.yDeadZone);
+			else if(this.followed.y - this.yDeadZone < this.yView)
+				this.yView = this.followed.y - this.yDeadZone;
+			}						
+		}		
 
-	}
-	if(this.axis == "vertical" || this.axis == "both")
-	{
-	// moves camera on vertical axis based on followed object position
-	if(this.followed.y - this.yView + this.yDeadZone > this.hView)
-		this.yView = this.followed.y - (this.hView - this.yDeadZone);
-	else if(this.followed.y - this.yDeadZone < this.yView)
-		this.yView = this.followed.y - this.yDeadZone;
-	}						
+		// update viewportRect
+		this.viewportRect.set(this.xView, this.yView);
 
-	}		
-
-	// update viewportRect
-	this.viewportRect.set(this.xView, this.yView);
-
-	// don't let camera leaves the world's boundary
-	if(!this.viewportRect.within(this.worldRect))
-	{
-		if(this.viewportRect.left < this.worldRect.left)
-			this.xView = this.worldRect.left;
-		if(this.viewportRect.top < this.worldRect.top)					
-			this.yView = this.worldRect.top;
-		if(this.viewportRect.right > this.worldRect.right)
-			this.xView = this.worldRect.right - this.wView;
-		if(this.viewportRect.bottom > this.worldRect.bottom)					
-			this.yView = this.worldRect.bottom - this.hView;
-	}
+		// don't let camera leaves the world's boundary
+		if (!this.viewportRect.within(this.worldRect)) {
+			if(this.viewportRect.left < this.worldRect.left)
+				this.xView = this.worldRect.left;
+			if(this.viewportRect.top < this.worldRect.top)					
+				this.yView = this.worldRect.top;
+			if(this.viewportRect.right > this.worldRect.right)
+				this.xView = this.worldRect.right - this.wView;
+			if(this.viewportRect.bottom > this.worldRect.bottom)					
+				this.yView = this.worldRect.bottom - this.hView;
+		}
 }
 
 function Player(x, y){
